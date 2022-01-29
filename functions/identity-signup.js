9ee692260d3c37834657efa1032d4138c225693d
Harsh Pareek <harshpareek@Harshs-MacBook-Air.local>
2022-01-29 13:47:37 +0530

    change

---
+++ functions/identity-signup.js
@@ -0,0 +1,37 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { faunaFetch } = require("./utils/fauna");
+
+exports.handler = async (event) => {
+  const { user } = JSON.parse(event.body);
+
+  const customer = await stripe.customers.create({ email: user.email });
+
+  await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
+  });
+
+  await faunaFetch({
+    query: `
+      mutation ($netlifyID: ID!, $stripeID: ID!) {
+        createUser(data: { netlifyID: $netlifyID, stripeID: $stripeID }) {
+          netlifyID
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.id,
+      stripeID: customer.id,
+    },
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      app_metadata: {
+        roles: ["free"],
+      },
+    }),
+  };
+};
